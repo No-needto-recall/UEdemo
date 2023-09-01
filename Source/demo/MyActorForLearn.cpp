ad18d4065b5a69741e7afaf4912da33b8ba3fafa
ctl <chen285@foxmail.com>
2023-09-01 17:45:40 +0800

    CPP

---
+++ Source/demo/MyActorForLearn.cpp
@@ -0,0 +1,101 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActorForLearn.h"
+#include "TimerManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+
+#include "MovieSceneSequenceID.h"
+
+// Sets default values
+AMyActorForLearn::AMyActorForLearn()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	//初始化一个静态网格体组件
+	this->Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MyMesh"));
+	//将Mesh设置为根节点
+	this->Mesh->SetupAttachment(this->RootComponent);
+#if 0
+	this->RootComponent = this->Mesh;
+#endif
+	//构造帮助
+	//静态是为了之后不需要重复加载
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> ResourcesAsset(TEXT("/Engine/BasicShapes/Cube.Cube"));
+	//判断资源加载是否成功
+	if(ResourcesAsset.Succeeded())
+	{
+		//设置静态网格体
+		this->Mesh->SetStaticMesh(ResourcesAsset.Object);
+		//额外的设置
+		this->Mesh->SetRelativeLocation(FVector(0.0f,0.0f,0.0f));
+		this->Mesh->SetRelativeScale3D(FVector(1.0f,1.0f,1.0f));
+	}
+
+	this->PitchValue = 0.0f;
+	this->YawValue= 0.0f;
+	this->RollValue= 0.0f;
+	
+}
+
+// Called when the game starts or when spawned
+void AMyActorForLearn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FTimerHandle Timer1;
+	FTimerHandle Timer2;
+	FTimerHandle Timer3;
+	this->GetWorldTimerManager().SetTimer(Timer1,this,&AMyActorForLearn::FunctionReapting,true,true);
+	this->GetWorldTimerManager().SetTimer(Timer2,this,&AMyActorForLearn::SimpleHarmonicMotion,0.017,true);
+	this->GetWorldTimerManager().SetTimer(Timer3,this,&AMyActorForLearn::MoveRotation,0.017,true);
+}
+
+// Called every frame
+void AMyActorForLearn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMyActorForLearn::Rotate()
+{
+	SetActorRotation(GetActorRotation().Add(PitchValue,YawValue,RollValue));
+	//解决传参四元素问题	
+	//FQuat quatRotation = FQuat();
+}
+
+void AMyActorForLearn::FunctionReapting()
+{
+	GEngine->AddOnScreenDebugMessage(-1,0.5,FColor::Yellow,TEXT("FunctionCall"));
+}
+
+void AMyActorForLearn::DebugInfo()
+{
+	GEngine->AddOnScreenDebugMessage(-1,100.f,FColor::Red,GetName());		
+	GEngine->AddOnScreenDebugMessage(-1,100.f,FColor::Red,GetClass()->GetName());
+
+	FVector Pos1 = UGameplayStatics::GetPlayerPawn(GetWorld(),0)->GetActorLocation();
+	FVector Pos2 = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
+	FVector Pos3 =  UGameplayStatics::GetPlayerController(GetWorld(),0)->GetPawn()->GetActorLocation();
+
+	DrawDebugPoint(GetWorld(),GetActorLocation(),50,FColor::Blue,true);
+	DrawDebugLine(GetWorld(),Pos1,GetActorLocation(),FColor::Blue,true);
+	DrawDebugDirectionalArrow(GetWorld(),Pos1,GetActorLocation(),100,FColor::Yellow,true);
+}
+
+void AMyActorForLearn::SimpleHarmonicMotion()
+{
+	float TotalTime = GetWorld()->GetTimeSeconds();
+	float sinNum = FMath::Sin(TotalTime*5.f);
+	FVector Location(0.0f,0.0f,5.0f);
+	Location *= sinNum;	
+	this->AddActorLocalOffset(Location);
+}
+
+void AMyActorForLearn::MoveRotation()
+{
+	this->AddActorLocalOffset(this->GetActorForwardVector()*10);
+	this->AddActorWorldRotation(FRotator(0.0f,1.0f,0.0f));
+}
+
---
+++ Source/demo/MyActorForLearn.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActorForLearn.generated.h"
+
+UCLASS()
+class DEMO_API AMyActorForLearn : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActorForLearn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//静态网格组件
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere,Category=Movement)
+	float PitchValue;
+	
+	UPROPERTY(EditAnywhere,Category=Movement)
+	float YawValue;
+	
+	UPROPERTY(EditAnywhere,Category=Movement)
+	float RollValue;
+
+	void Rotate();
+
+	void FunctionReapting();
+
+	void DebugInfo();
+
+	void SimpleHarmonicMotion();
+	
+	void MoveRotation();
+};
