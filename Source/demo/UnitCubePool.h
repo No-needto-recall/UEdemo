dd8754a4f5073491b21271ba0b694c8eaa470467
ctl <chen285@foxmail.com>
2023-09-21 17:06:01 +0800

    对象池

---
+++ Source/demo/UnitCubePool.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "UnitCubePool.generated.h"
+
+class AUnitCube;
+/**
+ * 
+ */
+UCLASS()
+class DEMO_API UUnitCubePool : public UObject
+{
+	GENERATED_BODY()
+private:
+	
+};
