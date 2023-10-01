b7545cf5da92750d911ab9b9d873443015110ba3
No-needto-recall <111343528+No-needto-recall@users.noreply.github.com>
2023-10-01 15:01:37 +0800

    自定义日志类

---
+++ Source/demo/MyCustomLog.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCustomLog.h"
+
+DEFINE_LOG_CATEGORY(MyCustomLog);
+
+
+#if 0
+	void Func()
+	{
+		CUSTOM_LOG_ERROR(TEXT("this is a custom error log"));	
+		CUSTOM_LOG_WARNING()(TEXT("this is a custom warning log"));	
+	}
+#endif
